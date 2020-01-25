1a04a6b0a2206756aa5a62348838a8be5e46fa61
Lenz Weber <lenz.weber@mayflower.de>
2020-01-25 22:09:45 +0100

    prep

---
+++ .postgraphilerc.js
@@ -0,0 +1,39 @@
+/**
+ * @type {import("./src").AuditPluginOptions}
+ */
+const auditPlugin = {
+  //auditEventConnection: false,
+  firstLastAuditEvent: false,
+  //dateProps: false,
+  //nameProps: false,
+  nameSource: "session_info",
+  nameSessionInfoJsonPath: "{userId}"
+};
+
+module.exports = {
+  options: {
+    graphileBuildOptions: {
+      auditPlugin
+    },
+    async pgSettings(req) {
+      const claims = getClaimsFromRequest(req);
+      return {
+        "pgmemento.session_info": JSON.stringify({
+          userId: claims.name
+        })
+      };
+    },
+    jwtSecret: "my-jwt-secret"
+  }
+};
+
+function getClaimsFromRequest(req) {
+  try {
+    const { authorization } = req.headers;
+    const [, token] = /^\s*bearer\s+(.*?)\s*$/i.exec(authorization);
+    const [, claims] = token.split(".");
+    return JSON.parse(Buffer.from(claims, "base64").toString());
+  } catch {
+    return {};
+  }
+}
---
+++ example-database.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS public.comments;
+DROP TABLE IF EXISTS public.posts;
+DROP TABLE IF EXISTS public.users;
+
+CREATE TABLE public.users (
+	id SERIAL PRIMARY KEY,
+	email TEXT NOT NULL
+);
+
+CREATE TABLE public.posts (
+	id SERIAL PRIMARY KEY,
+	user_id INT NOT NULL REFERENCES public.users(id),
+	headline TEXT NOT NULL,
+	content TEXT NOT NULL	
+);
+
+CREATE TABLE public.comments (
+	id SERIAL PRIMARY KEY,
+	user_id INT NOT NULL REFERENCES public.users(id),
+	post_id INT NOT NULL REFERENCES public.posts(id),
+	content TEXT NOT NULL
+);
